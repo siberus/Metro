e90ba409bc778c71d8c3aad6654f9f5003134b23
siberus <mr.siberus@yandex.ru>
2023-03-23 20:14:45 +0700

    Pretest

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 ﻿#include "pch.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
+    int a, b, n, m, tmin, tmax;
+    int tmina, tminb, tmaxa, tmaxb;
+    cin >> a >> b >> n >> m;
+
+    if (((a > b) && (n > m)) || ((a<b)&&(n<m))){
+            cout << -1 << endl;
+            return 0;
+    }
+        
+
+    tminb = (m - 1) * (1 + b) + 1;
+    tmaxa = n * (1 + a) + a;
+
+    tmina = (n - 1) * (1 + a) + 1;
+    tmaxb = m * (1 + b) + b;
+
+    cout << "tmina = " << tmina << " tmaxa=" << tmaxa << endl;
+    cout << "tminb = " << tminb << " tmaxb=" << tmaxb << endl;
+
+    if (tmina < tminb)
+        tmin = tminb;
+    else
+        tmin = tmina;
+
+    if (tmaxa < tmaxb)
+        tmax = tmaxa;
+    else
+        tmax = tmaxb;
+
+    cout << tmin << " " << tmax;
 
     return 0;
 }
